65493da37ca8f066d3de39258731b14c743252ad
kubistmi <kubistmi@gmail.com>
2018-09-09 21:59:37 +0200

    Week 1 script packages added.

--- w1/script1.R
+++ w1/script1.R
@@ -2,4 +2,5 @@
 # 1.) R & RStudio version
 # 2.) Overview: Source, console, History, Environment, Files, Packages, Help
 # 3.) Setup: Color, Positions, Best practise 
-# 4.) Git setup
+# 4.) Packages
+# 5.) Git setup
